3ad71bbfec6e4d036c084ae54e0e6ccf6759145c
calvin.cliff <calvin.cliff@impact-multimedia.com>
2024-10-24 11:13:06 +0800

    style: make the padding smaller around the image placeholder

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -58,7 +58,7 @@
       {/each}
     </div>
     <div
-      class="flex flex-col bg-violet-1000 gap-4 flex-1 p-4 justify-start lg:justify-center items-center"
+      class="flex flex-col bg-violet-1000 gap-4 flex-1 p-2 justify-start lg:justify-center items-center"
     >
       <img
         class="rounded-lg blueShadow"
