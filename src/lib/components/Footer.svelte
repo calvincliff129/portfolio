0aff24a7ddba56cc9d0639ff6f5d40fd350dab7f
calvin.cliff <calvin.cliff@impact-multimedia.com>
2024-10-25 09:42:08 +0800

    feat: adds link to linkedin and github anchor tag

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+  let linkedInLink: string = "https://www.linkedin.com/in/calvincliff2129";
+  let GithubLink: string = "https://github.com/calvincliff129";
+</script>
+
 <footer
   class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
 >
