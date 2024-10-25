dc6d62b01bd22671d61b1e756e80f0ba4827f909
calvin.cliff <calvin.cliff@impact-multimedia.com>
2024-10-25 09:29:49 +0800

    style: changes the border radius of the image

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -58,13 +58,9 @@
       {/each}
     </div>
     <div
-      class="flex flex-col bg-violet-1000 gap-4 flex-1 p-2 justify-start lg:justify-center items-center"
+      class="flex flex-col bg-violet-1000 gap-4 flex-1 p-1 justify-start lg:justify-center items-center"
     >
-      <img
-        class="rounded-lg blueShadow"
-        src={project.imgUrl}
-        alt="project-img"
-      />
+      <img class="rounded blueShadow" src={project.imgUrl} alt="project-img" />
     </div>
   </div>
 </div>
